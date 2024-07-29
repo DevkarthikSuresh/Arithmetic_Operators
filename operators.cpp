7d03118a684c43c8697a8370f6efbc7390cd439e
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-29 20:34:32 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,24 @@
+//Name - Devarthik Suresh
+//PRN - 23070123045
+//Aim - Adding 2 numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cout<<"Enter num 1: "<<endl;
+    cin>>a;
+    cout<<"Enter num 2: "<<endl;
+    cin>>b;
+    c = a+b;
+    cout<<"The No. is: "<<c;
+
+}
+/* Output-
+Enter num 1:
+5
+Enter num 2:
+5
+The No. is: 10*/
